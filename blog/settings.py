dff21f5de0842b7800168383b70ab4d37695a826
arthur salatine <sawurora@gmail.com>
2022-02-27 17:00:59 -0300

    STATIC_ROOT defined on settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = (BASE_DIR / 'templates/static',)
 
 MEDIA_ROOT = BASE_DIR / 'media'
