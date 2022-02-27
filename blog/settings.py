b51b7d269138d4e0702d6737190c94b4cb005f22
arthur salatine <sawurora@gmail.com>
2022-02-27 17:18:05 -0300

    allowed_hosts

--- blog/settings.py
+++ blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--!uglsr%kqt00^t2t46_+qglll1h-e1)ny4ssg*003_nq+rw4g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://blognoticias.herokuapp.com/']
 
 
 # Application definition
